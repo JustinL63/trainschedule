2e1bcca3b7687093028143927b249e4bd2afcf77
Justin Lamb <lambjg63@gmail.com>
2018-11-28 09:18:16 -0500

    something

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -6,24 +6,28 @@
 $("#submit").on("click", function(event) {
     event.preventDefault();
 
+    //creating variables from user input
+
     var trname = $("#trainname").val().trim();
     var trndest = $("#traindestination").val().trim();
     var trntime = moment($("#firsttime").val().trim(), "MM/DD/YYYY").format("X");
     var trnfreq = $("#trainfrequency").val().trim();
 
+    //creating an object from the variable entered by the user
     var newtrain = {
         Name: trname,
         Destination: trndest,
         Time: trntime,
         Frequency: trnfreq
     };
+    //pushing the new train to the firebase data base
     database.ref().push(newtrain);
 
     console.log(newtrain.Name);
     console.log(newtrain.Destination);
     console.log(newtrain.Time);
     console.log(newtrain.Frequency);
-
+// creating a new row for the train added to the schedule
     var newRow = $("<tr>").append(
         $("<td>").text(trname),
         $("<td>").text(trndest),
